022ca668f444c646e844fa68e30bcc3953fd1660
GeekMind2000 <mostafaayad67@gmail.com>
2020-05-17 06:03:47 +0200

    pgm read-write version2.0

--- pgm read-write/test.cpp
+++ pgm read-write/test.cpp
@@ -1,11 +1,20 @@
 #include <bits/stdc++.h>
-#include "pgmb_io.hpp"
+#include "pgmb_io_V2.hpp"
 using namespace std;
 int main()
 {
-    string input_name = "NORMAL2-IM-1440-0001.pgm", output_name = "gmdan";
+    string input_name = "NORMAL2-IM-1440-0001.pgm", output_name = "NORMAL2-IM-1427-0001.pgm";
     int xsize, ysize;
-    unsigned char maxg, *g;
-    cout << pgmb_read(input_name, xsize, ysize, maxg, &g) << pgmb_write(output_name, xsize, ysize, g);
+    unsigned char maxg;
+    vector<int> data;
+    cout << pgmb_read(input_name, xsize, ysize, maxg, data) << endl;
+    cout << xsize << " " << ysize << endl ;
+    //for (int i = 0; i < data.size(); ++i) cout << data[i] << " ";
+    cout << endl
+         << pgmb_write(output_name, xsize, ysize, data);
+    //cout<<endl<<data.size()<<endl;
+    // for(int i=0;i<data.size();++i) cout<<data[i]<<" ";
+    // for(int i=0;i<data.size();++i) if(data[i]>255||data[i]<0) cout<<data[i]<<endl;
+
     return 0;
 }
