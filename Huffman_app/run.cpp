77b0d705433615fc4ec3963af97eb915cf084c48
GeekMind2000 <mostafaayad67@gmail.com>
2020-05-26 03:30:23 +0200

    huffman encoding done

--- Huffman_app/run.cpp
+++ Huffman_app/run.cpp
@@ -7,7 +7,7 @@ int main()
 {
     string input_name = "NORMAL2-IM-1440-0001.pgm", output_name = "NORMAL2-IM-1427-0001.pgm", comp_name = "yarab";
     pgm pic;
-    cout << pgmb_read(input_name, pic)<<" SUCCESSFUL READ <3"<<endl;
+    cout << pgmb_read(input_name, pic)<<" ERRORS,SUCCESSFUL READ <3"<<endl;
     //  cout<< pgmb_write(output_name, xsize, ysize, data);
 
 unordered_map<int, int> frequencyTable;
