e0718ab14e2818b37358fd082298df3588504ca1
GeekMind2000 <mostafaayad67@gmail.com>
2020-05-27 03:00:00 +0200

    read/write pgm V3

--- Huffman_app/run.cpp
+++ Huffman_app/run.cpp
@@ -7,16 +7,16 @@ int main()
 {
     string input_name = "NORMAL2-IM-1440-0001.pgm", output_name = "NORMAL2-IM-1427-0001.pgm", comp_name = "yarab";
     pgm pic;
-    cout << pgmb_read(input_name, pic)<<" ERRORS,SUCCESSFUL READ <3"<<endl;
-    //  cout<< pgmb_write(output_name, xsize, ysize, data);
+    cout << pgmb_read(input_name, pic) << " ERRORS,SUCCESSFUL READ <3" << endl;
+    //  cout<< pgmb_write(output_name, pic);
 
-unordered_map<int, int> frequencyTable;
-buildFreqTable( pic.data, frequencyTable);
+    unordered_map<int, int> frequencyTable;
+    buildFreqTable(pic.data, frequencyTable);
 
     Huffman compressor;
-    
+
     auto codes = compressor.Encode(frequencyTable);
-   for (auto &x : codes)
+    for (auto &x : codes)
         cout << x.first << ": " << x.second << '\n';
     return 0;
 }
