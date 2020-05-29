abae4a774be491ab5eeaac881a661cd0c5c6cc94
mmn420 <momen99223@gmail.com>
2020-05-30 01:43:15 +0200

    test height

---
+++ Huffman_app/deseriallize.hpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+void Deseriallize_pgm(string filename)
+{
+    ifstream deseriallize;
+    deseriallize.open(filename);
+    if (!deserialize.is_open())
+    {
+        cout << "FILE DID NOT OPEN PROPERLY" << endl;
+        return;
+    }
+    //deseriallzing width and height
+        string height = "";
+        string height_final_bits = "";
+    for (i = 0; i < 4; i++)
+    {
+        deseriallize.get(height[i]);
+        int temp_h = (int) height[i];
+        height_final_bits += bitset<8> height_bits(temp_h).to_string();
+    }
+    int HEIGHT = (int) bitset<32> final_height(height_final_bits).to_ulong();
+    string width = "";
+    cout<<HEIGHT<<endl;
+    deseriallize.close();
+   /* for (i = 0; i < 4; i++) 
+    {
+        deseriallize.get(width[i]);
+    } */
+}
--- Huffman_app/run.cpp
+++ Huffman_app/run.cpp
@@ -2,10 +2,11 @@
 #include "utility.hpp"
 #include "pgmb_io_V3.hpp"
 #include "Huffman_encoding.hpp"
+#include "deseriallize.hpp"
 using namespace std;
 int main()
 {
-    string input_name = "NORMAL2-IM-1442-0001.pgm", output_name = "NORMAL2-IM-1427-0001.pgm", encoded_pgm = "encoded_pgm.txt",freq_table="yarabb";
+    string input_name = "NORMAL2-IM-1442-0001.pgm", output_name = "NORMAL2-IM-1427-0001.pgm", encoded_pgm = "encoded_pgm.txt",freq_table="yarabb", decoded_pgm="test.txt"; 
     pgm pic;
     cout << pgmb_read(input_name, pic) << " ERRORS,SUCCESSFUL READ <3" << endl;
     //  cout<< pgmb_write(output_name, pic);
@@ -16,6 +17,7 @@ int main()
     Huffman compressor;
     auto codes = compressor.Encode(frequencyTable);
     serializePgm(pic,codes,encoded_pgm);
+    Deseriallize_pgm(decoded_pgm);
   /*for (auto &x : codes)
         cout << x.first << ": " << x.second << '\n';*/
 
