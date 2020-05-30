6f5b5c45a8e6408e9aa5c79f6a7d37fd6b1b69bc
mmn420 <momen99223@gmail.com>
2020-05-30 02:04:43 +0200

    hmm

--- Huffman_app/deseriallize.hpp
+++ Huffman_app/deseriallize.hpp
@@ -10,19 +10,19 @@ void Deseriallize_pgm(string filename)
         return;
     }
     //deseriallzing width and height
-        string height = "";
-        string height_final_bits = "";
+    string height = "";
+    string height_final_bits = "";
     for (i = 0; i < 4; i++)
     {
         deseriallize.get(height[i]);
-        int temp_h = (int) height[i];
+        int temp_h = (int)height[i];
         height_final_bits += bitset<8> height_bits(temp_h).to_string();
     }
-    int HEIGHT = (int) bitset<32> final_height(height_final_bits).to_ulong();
+    int HEIGHT = (int)bitset<32> final_height(height_final_bits).to_ulong();
     string width = "";
-    cout<<HEIGHT<<endl;
+    cout << HEIGHT << endl;
     deseriallize.close();
-   /* for (i = 0; i < 4; i++) 
+    /* for (i = 0; i < 4; i++) 
     {
         deseriallize.get(width[i]);
     } */
