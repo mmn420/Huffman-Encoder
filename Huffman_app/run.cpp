d4584ac9d53b532e2459bf61a5c7457fba663ec1
mmn420 <momen99223@gmail.com>
2020-06-02 12:49:54 +0200

    finished flags

--- Huffman_app/run.cpp
+++ Huffman_app/run.cpp
@@ -3,43 +3,114 @@
 #include "pgmb_io_V3.hpp"
 #include "Huffman_encoding.hpp"
 using namespace std;
-int main()
+
+int main(int argc, char *argv[])
 {
-    string input_name = "1.pgm", output_name = "new_pic.pgm", encoded_pgm = "encoded_pgm.txt", freq = "freq.txt", decoded_pgm = "decoded_pgm.txt";
-    pgm read_pic;
-    cout << pgmb_read(input_name, read_pic) << " ERRORS,SUCCESSFUL READ <3" << endl;
-    unordered_map<int, int> frequencyTable;
-    buildFreqTable(read_pic.data, frequencyTable);
-     unordered_map<int, int> tempTable;
-    for (int i = 0; i < 256; i++)
-        if (frequencyTable.count(i))
-            tempTable[i] = frequencyTable[i];
 
-    frequencyTable = tempTable;
-    Huffman compressor;
-    auto codes = compressor.Encode(frequencyTable);
-    serializePgm(read_pic, codes, encoded_pgm);
-    serializeFreq(frequencyTable, freq);
+    if (argc == 2)
+    {
+        string imageName = argv[1];
 
+        if (imageName.length() < 4)
+        {
+            cout << "Bad file name\n";
+            return -1;
+        }
+
+        if (imageName.substr(imageName.length() - 4, 4) != ".pgm")
+        {
+            cout << "Invalid file extension\n";
+            return -1;
+        }
+
+        string imageNameNoExtension = imageName.substr(0, imageName.length() - 4);
+
+        pgm read_pic;
+        int errorCount = pgmb_read(imageName, read_pic);
+        if (errorCount)
+        {
+            cout << errorCount << " errors encountered\n";
+            return -1;
+        }
+
+        unordered_map<int, int> frequencyTable;
+        buildFreqTable(read_pic.data, frequencyTable);
+        unordered_map<int, int> tempTable;
+        for (int i = 0; i < 256; i++)
+            if (frequencyTable.count(i))
+                tempTable[i] = frequencyTable[i];
+
+        frequencyTable = tempTable;
+        Huffman compressor;
+        auto codes = compressor.Encode(frequencyTable);
+        serializePgm(read_pic, codes, imageNameNoExtension + ".enc");
+        serializeFreq(frequencyTable, imageNameNoExtension + ".frq");
+    }
     //*******************************************************
-    pgm write_pic;
-    Huffman decompressor;
-    string encodedData = "";
-    deserializePgm(encoded_pgm, write_pic, encodedData);
-    unordered_map<int, int> dFrequencyTable;
-    deserializeFreq(freq, dFrequencyTable);
-  //  auto dCodes = decompressor.Encode(frequencyTable);
-   /* for(int i=0;i<256;++i)
-    if(dCodes[i]!=codes[i]) cout<<dCodes[i]<<" "<<codes[i]<<endl;*/
-   write_pic.data = decompressor.Decode(encodedData, dFrequencyTable);
-     // cout<<write_pic.data.size()<<endl;
-   cout << pgmb_write(output_name, write_pic) << " ERRORS,SUCCESSFUL WRITE <3" << endl;
-   /* for(int i=0;i<256;++i)
+    else if (argc == 4)
+    {
+        int flagPos = -1;
+
+        for (int i = 1; i <= 3; i++)
+            if (strcmp(argv[i], "-t") == 0)
+                flagPos = i;
+
+        if (flagPos == -1 || flagPos == 3)
+        {
+            cout << "Invalid arguments, did you forget/misplace -t flag?\n";
+            return -1;
+        }
+
+        string encoded_pgm;
+        string freq;
+        string output_name;
+
+        freq = argv[flagPos + 1];
+
+        encoded_pgm = (flagPos == 1) ? argv[3] : argv[1];
+
+        if (encoded_pgm.length() < 4 || freq.length() < 4)
+        {
+            cout << "Bad file name\n";
+            return -1;
+        }
+
+        if (encoded_pgm.substr(encoded_pgm.length() - 4, 4) != ".enc" || freq.substr(freq.length() - 4, 4) != ".frq")
+        {
+            cout << "Invalid file extension\n";
+            return -1;
+        }
+
+        output_name = encoded_pgm.substr(0, encoded_pgm.length() - 4) + ".pgm";
+
+        pgm write_pic;
+        Huffman decompressor;
+        string encodedData = "";
+        deserializePgm(encoded_pgm, write_pic, encodedData);
+        unordered_map<int, int> dFrequencyTable;
+        deserializeFreq(freq, dFrequencyTable);
+        //  auto dCodes = decompressor.Encode(frequencyTable);
+        /* for(int i=0;i<256;++i)
+        if(dCodes[i]!=codes[i]) cout<<dCodes[i]<<" "<<codes[i]<<endl;*/
+        write_pic.data = decompressor.Decode(encodedData, dFrequencyTable);
+        // cout<<write_pic.data.size()<<endl;
+        int errorCount = pgmb_write(output_name, write_pic);
+        if (errorCount)
+        {
+            cout << errorCount << " errors encountered\n";
+            return -1;
+        }
+    }
+    else
+    {
+        cout << "Invalid arguments specified\n";
+    }
+    return 0;
+    /* for(int i=0;i<256;++i)
     cout<<frequencyTable[i]<<":"<<dFrequencyTable[i]<<endl;
     cout<<frequencyTable.size()<<endl<<dFrequencyTable.size()<<endl;*/
     /* for (auto &x : codes)
         cout << x.first << ":" << x.second << " ";*/
     /*for (auto &x : frequencyTable)
         cout << x.first << ":" << x.second << " ";*/
-    return 0;
 }
